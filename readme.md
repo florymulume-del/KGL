761c408636f37d4d43bbdbc015bfabb52ead49df
florymulume-del <florymulume@gmail.com>
2026-01-07 22:49:03 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+## ESLint & Prettier Setup
+
+### Issues Fixed
+- Inconsistent indentation
+- Missing semicolons
+- Unused variables
+- Mixed quote styles
+- Trailing whitespace
+- Fixed unused variables
+- Fixed undefined functions
+- Replaced == with ===
+- Removed unused imports
+- Formatted code using Prettier
+
+### Tools Used
+- ESLint
+- Prettier
+- VS Code Format on Save
+
---
+++ script.js
@@ -0,0 +1,21 @@
+const loginTab = document.getElementById('loginTab');
+const signupTab = document.getElementById('signupTab');
+const nameField = document.getElementById('nameField');
+const submitBtn = document.getElementById('submitBtn');
+const forgotLink = document.getElementById('forgotLink');
+
+loginTab.onclick = () => {
+  loginTab.classList.add('active');
+  signupTab.classList.remove('active');
+  nameField.classList.add('hidden');
+  forgotLink.style.display = 'inline';
+  submitBtn.textContent = 'Login';
+};
+signupTab.onclick = () => {
+  signupTab.classList.add('active');
+  loginTab.classList.remove('active');
+
+  nameField.classList.remove('hidden');
+  forgotLink.style.display = 'none';
+  submitBtn.textContent = 'Create Account';
+};
